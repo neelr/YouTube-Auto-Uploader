9e679c92fc217eb207507b371b4caf5fff5a99dd
Neel Redkar <neel.redkar@outlook.com>
2020-07-02 16:08:56 -0700

    add basic testing for ranges

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from utils.spreadsheet import Spreadsheet
+import os
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 
 
 @app.route('/')
 def root():
-    sheet = Spreadsheet()
+    sheet = Spreadsheet(os.getenv("SPREADSHEET_ID"))
+    sheet.getAll()
     return "YouTube Auto Uploader in action 💥!"
 
 if __name__ == '__main__':
--- utils/spreadsheet.py
+++ utils/spreadsheet.py
@@ -1,3 +1,44 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+"""
+Class to interface with spreadsheet in an easy way
+"""
 class Spreadsheet:
-    def __init__(self):
-        print("start")
+    def __init__(self, id):
+        self.SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+        self.ID = id
+
+        self.oauth()
+    def oauth(self):
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        self.sheet = service.spreadsheets()
+    def getAll(self):
+        result = self.sheet.values().get(spreadsheetId=self.ID, range="Main!A1:C3").execute()
+        values = result.get('values', [])
+
+        print(values)
